6ac16d8ebbd589712832921aaf9760c44f9aa6e2
Egor <egorrakov50@gmail.com>
2025-04-08 14:58:03 +0700

    vhange

---
+++ app/components/B.tsx
@@ -0,0 +1,5 @@
+export default function B() {
+    return (
+        <>brave new world</>
+    )
+}
---
+++ app/components/FooNumber.tsx
@@ -0,0 +1,10 @@
+import Bar from "./Bar";
+
+export default function FooNumber() {
+    return (
+        <span>
+        <>20</>
+        <Bar />
+        </span>
+    )
+}
